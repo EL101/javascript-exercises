d331283d1565d0dc8c34f822c5d1800bcc648453
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-16 11:41:33 -0400

    Complete 08-removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let remove = [];
+    for (let i = 1; i < arguments.length; i++) {
+        remove.push(arguments[i]);
+    }
+    // console.log(arguments);
+    return arr.filter(curr => !remove.includes(curr));
 };
-
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
