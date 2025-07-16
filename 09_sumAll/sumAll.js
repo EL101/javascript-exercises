8deb55f73a1752f23be0c9df63718c1eb3500a5c
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-16 11:47:42 -0400

    Complete 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || Math.floor(a) !== a || Math.floor(b) !== b) {
+        return "ERROR";
+    }
 
+    return (a + b) * (Math.abs(b - a) + 1) / 2;
 };
 
 // Do not edit below this line
