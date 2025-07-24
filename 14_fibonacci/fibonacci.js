030802c2058394adca2ee27cd9bef083f6f51869
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-24 12:14:51 -0400

    Complete 14-fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = parseFloat(a);
+    if (a<0) {
+        return "OOPS";
+    }
+    let dp = [0, 1];
+    for (let i = 2; i <= a; i++) {
+        dp.push(dp[i-1] + dp[i-2]);
+    }
+    return dp[a];
 };
 
 // Do not edit below this line
