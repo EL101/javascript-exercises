5de565b12548e230c85efa06c4ad12728b4f926d
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-24 12:25:46 -0400

    Complete 16-findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sorted = people.sort((a, b) => {
+        let ageA, ageB;
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = 2025;
+        }
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = 2025;
+        }
+        ageA = a.yearOfDeath - a.yearOfBirth;
+        ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageA - ageB;
+    });
+    // console.log(people[people.length - 1].name);
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
