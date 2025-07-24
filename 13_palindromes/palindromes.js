fa908f789a2488f98ca0e2605c8fa0c18ef81468
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-24 12:09:54 -0400

    Complete 13-palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newStr = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string.at(i).toUpperCase() != string.at(i).toLowerCase()) {
+            newStr += string.at(i).toLowerCase();
+        } else if (!isNaN(parseFloat(string.at(i)))) {
+            newStr += string.at(i);
+        }
+    }
+    console.log(newStr);
+    for (let i = 0; i < Math.floor(newStr.length / 2); i++) {
+        if (newStr.at(i)!=newStr.at(-(i+1))) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
